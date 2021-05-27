a1e4548916e23fb9d6a9751f59590f4c4210e99d
Hauiming Yu <Huaimingyuuu@gmail.com>
2021-05-27 17:39:52 +0800

    reset package structure

---
+++ quanestimation/Common/Liouville.jl
@@ -0,0 +1,42 @@
+module Liouville
+function liouville_commu(A::Array{T}) where {T <: Complex}
+    dim = size(A)[1]
+    result = zeros(T, dim^2, dim^2)
+    @inbounds for i in 1:dim
+        @inbounds for j in 1:dim
+            @inbounds for k in 1:dim
+                ni = dim * (i - 1) + j
+                nj = dim * (k - 1) + j
+                nk = dim * (i - 1) + k
+
+                result[ni,nj] = A[i,k]
+                result[ni,nk] = -A[k,j]
+                result[ni,ni] = A[i,i] - A[j,j]
+            end
+        end
+    end
+    result
+end
+
+function liouville_dissip(A::Array{T}) where {T <: Complex}
+    dim = size(A)[1]
+    result =  zeros(T, dim^2, dim^2)
+    @inbounds for i = 1:dim
+        @inbounds for j in 1:dim
+            ni = dim * (i - 1) + j
+            @inbounds for k in 1:dim
+                @inbounds for l in 1:dim 
+                    nj = dim * (k - 1) + l
+                    L_temp = A[i,k] * conj(A[j,l])
+                    @inbounds for p in 1:dim
+                        L_temp -= 0.5 * float(k == i) * A[p,j] * conj(A[p,l]) + 0.5 * float(l == j) * A[p,k] * conj(A[p,i])
+                    end
+                    result[ni,nj] = L_temp
+                end
+            end 
+        end
+    end
+    result[findall(abs.(result) .< 1e-10)] .= 0.
+    result
+end
+end
