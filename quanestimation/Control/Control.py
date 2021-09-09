d5af9276700cb3a1945db1497a63e33910766fac
Huaiming Yu <Huaimingyuuu@gmail.com>
2021-09-09 14:35:05 +0800

    revise input dtype

--- quanestimation/Control/Control.py
+++ quanestimation/Control/Control.py
@@ -47,12 +47,12 @@ class ControlSystem:
         """   
         
         self.tspan = tspan
-        self.rho_initial = rho_initial.view(dtype=np.complex128)
-        self.freeHamiltonian = H0.view(dtype=np.complex128)
-        self.control_Hamiltonian = [x.view(dtype=np.complex128) for x in Hc]
-        self.Hamiltonian_derivative = [x.view(dtype=np.complex128) for x in dH]
+        self.rho_initial = np.array(rho_initial,dtype=np.complex128)
+        self.freeHamiltonian = np.array(H0,dtype=np.complex128)
+        self.control_Hamiltonian = [np.array(x,dtype=np.complex128) for x in Hc]
+        self.Hamiltonian_derivative = [np.array(x,dtype=np.complex128) for x in dH]
         self.control_coefficients = ctrl_initial
-        self.Liouville_operator = [x.view(dtype=np.complex128) for x in Liouville_operator]
+        self.Liouville_operator = [np.array(x, dtype=np.complex128) for x in Liouville_operator]
         self.gamma = gamma
         self.control_option = control_option
         
