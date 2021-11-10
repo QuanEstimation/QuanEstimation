e2c8895a00deb30be98eb89fbbf4db1c91a5d64f
HuaiMing Yu <huaimingyuuu@gmail.com>
2021-11-10 14:51:09 +0800

    Update __init__.py

--- quanestimation/__init__.py
+++ quanestimation/__init__.py
@@ -9,9 +9,9 @@ from quanestimation import Resources
 from quanestimation import StateOptimization
 
 from quanestimation.AsymptoticBound import (CramerRao, Holevo, CFIM, LLD, QFIM, RLD, SLD, )
-from quanestimation.Common import (common, dRHO, dydt, mat_vec_convert, suN_generator, )
-from quanestimation.Control import (DDPG, DiffEvo, GRAPE, PSO, )
-from quanestimation.Dynamics import (dynamics, learning_env, Lindblad,  env, )
+from quanestimation.Common import (common, mat_vec_convert, suN_generator, )
+from quanestimation.Control import (ControlOpt, DDPG, DiffEvo, GRAPE, PSO, )
+from quanestimation.Dynamics import (dynamics, Lindblad,  )
 from quanestimation.Resources import (Resources, )
 from quanestimation.StateOptimization import (StateOpt_DE, StateOpt_PSO, StateOpt_NM, StateOpt_AD, StateOpt, StateOptSystem)
 
