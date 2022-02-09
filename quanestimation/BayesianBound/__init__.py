7e9ca408a38621e9cebb279592f9c484d111a579
HuaiMing <huaimingyuuu@gmail.com>
2022-02-09 19:18:58 +0800

    QZZB

--- quanestimation/BayesianBound/__init__.py
+++ quanestimation/BayesianBound/__init__.py
@@ -1,6 +1,8 @@
 
 from quanestimation.BayesianBound import BayesianCramerRao
+from quanestimation.BayesianBound import ZivZakai 
 
 from quanestimation.BayesianBound.BayesianCramerRao import (BQCRB, OBB, TWC)
+from quanestimation.BayesianBound.ZivZakai import (QZZB,)
 
-__all__ = ["BQCRB", "OBB", "TWC"]
+__all__ = ["BQCRB", "OBB", "TWC", "QZZB"]
