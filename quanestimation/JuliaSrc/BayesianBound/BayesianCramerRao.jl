ad3c4197a991e71339c84b58781dcb92ec985f1d
HuaiMing <huaimingyuuu@gmail.com>
2022-02-19 19:13:13 +0800

    bayesian jl

--- quanestimation/JuliaSrc/BayesianBound/BayesianCramerRao.jl
+++ quanestimation/JuliaSrc/BayesianBound/BayesianCramerRao.jl
@@ -1,76 +1,28 @@
 
 ########## Bayesian quantum Cramer-Rao bound ##########
-function BCRB(x, p, rho, drho; M=[], b=[], db=[], btype=1, eps=1e-8)
+function BQCRB(x, p, rho, drho; b=[], db=[], btype=1, eps=1e-8)
+        
     para_num = length(x)
-    p_num = length(p)
 
     if b==[]
-        b = [zeros(p_num) for i in 1:para_num]
-        db = [zeros(p_num) for i in 1:para_num]
+        b =  [zero(x) for x in x]
+        db =  [zero(x) for x in x]
     end
     if b!=[] && db==[] 
-        db = [zeros(p_num) for i in 1:para_num]
+        db = [zero(x) for x in x]
     end
 
-    if para_num == 1
-        #### singleparameter senario ####
-        if M==[]
-            M = load_M(size(rho[1])[1])
-        end
-        if typeof(drho[1]) == Vector{Matrix{ComplexF64}}
-            drho = [drho[i][1] for i in 1:p_num]
-        end
-        if typeof(b[1]) == Vector{Float64} || typeof(b[1]) == Vector{Int64}
-            b = b[1]
-        end
-        if typeof(db[1]) == Vector{Float64} || typeof(db[1]) == Vector{Int64}
-            db = db[1]
-        end
-        F_tp = zeros(p_num)
-        for i in 1:p_num
-            f = CFI(rho[i], drho[i], M; eps=eps)
-            F_tp[i] = f
-        end
-        F = 0.0
-        if btype == 1
-            arr = [p[i]*((1+db)^2/F_tp[i]+b^2) for i in 1:p_num]
-            F = trapz(x[1], arr)
-        elseif btype == 2
-            arr = [p[i]*F_tp[i] for i in 1:p_num]
-            F1 = trapz(x[1], arr)
-            arr2 = [p[j]*(1+db[j]) for j in 1:p_num]
-            B = trapz(x[1], arr2)
-            arr3 = [p[k]*b[k]^2 for k in 1:p_num]
-            B1 = trapz(x[1], arr3)
-            F = B^2*(1.0/F1)+B1^2
-        end
-        return F
-    else
-        #### multiparameter senario ####
-        println("pass")
-    end
-end
-
-function BQCRB(x, p, rho, drho; b=[], db=[], dtype="SLD", btype=1, eps=1e-8)
-    para_num = length(x)
-    p_num = length(p)
-
-    if b==[]
-        b = [zeros(p_num) for i in 1:para_num]
-        db = [zeros(p_num) for i in 1:para_num]
-    end
-    if b!=[] && db==[] 
-        db = [zeros(p_num) for i in 1:para_num]
-    end
-    if para_num == 1
+    
+    if para_num == 1    
+        p_num = length(p)
         #### singleparameter senario ####
         if typeof(drho[1]) == Vector{Matrix{ComplexF64}}
             drho = [drho[i][1] for i in 1:p_num]
         end
-        if typeof(b[1]) == Vector{Float64} || typeof(b[1]) == Vector{Int64}
+        if typeof(b) == Vector{Float64} || typeof(b) == Vector{Int64}
             b = b[1]
         end
-        if typeof(db[1]) == Vector{Float64} || typeof(db[1]) == Vector{Int64}
+        if typeof(db) == Vector{Float64} || typeof(db) == Vector{Int64}
             db = db[1]
         end
         F_tp = zeros(p_num)
@@ -85,24 +37,170 @@ function BQCRB(x, p, rho, drho; b=[], db=[], dtype="SLD", btype=1, eps=1e-8)
         elseif btype == 2
             arr = [p[i]*F_tp[i] for i in 1:p_num]
             F1 = trapz(x[1], arr)
-            arr2 = [p[j]*(1+db[j]) for j in 1:p_num]
-            B = trapz(x[1], arr2)
-            arr3 = [p[k]*b[k]^2 for k in 1:p_num]
-            B1 = trapz(x[1], arr3)
-            F = B^2*(1.0/F1)+B1^2
+            F = (1+db)^2*(1.0/F1)+b^2
         end
         return F
     else
         #### multiparameter senario ####
-        println("pass")
+        xnum = length(x)
+        bs  =  Iterators.product(b...)
+        dbs =  Iterators.product(db...)
+
+        trapzm(x, integrands, slice_dim) =  [trapz(tuple(x...), I) for I in [reshape(hcat(integrands...)[i,:], length.(x)...) for i in 1:slice_dim]] 
+
+        if btype == 1 
+            integrand(p,rho,drho,b,db)=p*diagm(1 .+db)*pinv(QFIM(rho,drho,eps))*diagm(1 .+db)+b*b'
+
+            integrands = [integrand(p,rho,drho,[b...],[db...])|>vec for (p,rho,drho,b,db) in zip(p,rho,drho,bs,dbs)]
+
+            I = trapzm(x, integrands, xnum^2) |> I->reshape(I,xnum,xnum)
+
+        elseif btype == 2
+            Bs = [p*(1 .+[db...]) for (p,db) in zip(p,dbs)]
+            B = trapzm(x, Bs, xnum)|> diagm
+
+            Fs = [p*QFIM(rho,drho,eps)|>vec for (p,rho,drho) in zip(p,rho,drho)]
+            F = trapzm(x, Fs, xnum^2) |> I->reshape(I,xnum,xnum)
+
+            bbts = [p*[b...]*[b...]'|>vec for (p,b) in zip(p,bs)]
+            I = B*pinv(F)*B + (trapzm(x, bbts, xnum^2) |> I->reshape(I,xnum,xnum))
+        end
+        return I
+    end
+
+end
+
+function BCRB(x, p, rho, drho; M=[], b=[], db=[], btype=1, eps=1e-8)
+    para_num = length(x)
+
+    
+    if b==[]
+        b =  [zero(x) for x in x]
+        db =  [zero(x) for x in x]
+    end
+    if b!=[] && db==[] 
+        db = [zero(x) for x in x]
+    end
+
+    
+    if para_num == 1
+        p_num = length(p)
+        #### singleparameter senario ####
+        if typeof(drho[1]) == Vector{Matrix{ComplexF64}}
+            drho = [drho[i][1] for i in 1:p_num]
+        end
+        if typeof(b) == Vector{Float64} || typeof(b) == Vector{Int64}
+            b = b[1]
+        end
+        if typeof(db) == Vector{Float64} || typeof(db) == Vector{Int64}
+            db = db[1]
+        end
+        F_tp = zeros(p_num)
+        for i in 1:p_num
+            f = QFIM(rho[i], drho[i]; dtype=dtype, eps=eps)
+            F_tp[i] = f
+        end
+        F = 0.0
+        if btype == 1
+            arr = [p[i]*((1+db)^2/F_tp[i]+b^2) for i in 1:p_num]
+            F = trapz(x[1], arr)
+        elseif btype == 2
+            arr = [p[i]*F_tp[i] for i in 1:p_num]
+            F1 = trapz(x[1], arr)
+            F = (1+db)^2*(1.0/F1)+b^2
+        end
+        return F
+    else
+        #### multiparameter senario #### # TODO: load_M for multi-
+        xnum = length(x)
+        bs  =  Iterators.product(b...)
+        dbs =  Iterators.product(db...)
+
+        trapzm(x, integrands, slice_dim) =  [trapz(tuple(x...), I) for I in [reshape(hcat(integrands...)[i,:], length.(x)...) for i in 1:slice_dim]] 
+
+        if btype == 1 
+            integrand(p,rho,drho,b,db)=p*diagm(1 .+db)*pinv(CFIM(rho,drho,M,eps))*diagm(1 .+db)+b*b'
+
+            integrands = [integrand(p,rho,drho,[b...],[db...])|>vec for (p,rho,drho,b,db) in zip(p,rho,drho,bs,dbs)]
+
+            I = trapzm(x, integrands, xnum^2) |> I->reshape(I,xnum,xnum)
+
+        elseif btype == 2
+            Bs = [p*(1 .+[db...]) for (p,db) in zip(p,dbs)]
+            B = trapzm(x, Bs, xnum)|> diagm
+
+            Fs = [p*CFIM(rho,drho,M,eps)|>vec for (p,rho,drho) in zip(p,rho,drho)]
+            F = trapzm(x, Fs, xnum^2) |> I->reshape(I,xnum,xnum)
+
+            bbts = [p*[b...]*[b...]'|>vec for (p,b) in zip(p,bs)]
+            I = B*pinv(F)*B + (trapzm(x, bbts, xnum^2) |> I->reshape(I,xnum,xnum))
+        end    
+        return I
+    end
+
+end
+
+function QVTB(x, p, dp, rho, drho; dtype="SLD", btype=1, eps=1e-8)
+    para_num = length(x)
+    if para_num == 1
+        #### singleparameter senario ####
+        p_num = length(p)
+        if typeof(drho[1]) == Vector{Matrix{ComplexF64}}
+            drho = [drho[i][1] for i in 1:p_num]
+        end
+        if typeof(dp[1]) == Vector{Float64}
+            dp = [dp[i][1] for i in 1:p_num]
+        end
+        F_tp = zeros(p_num)
+        for m in 1:p_num
+            F_tp[m] = QFIM(rho[m], drho[m]; dtype=dtype, eps=eps)
+        end
+        res = 0.0
+        if btype==1
+            I_tp = [real(dp[i]*dp[i]/p[i]^2) for i in 1:p_num]
+            arr = [p[j]/(I_tp[j]+F_tp[j]) for j in 1:p_num]
+            I = trapz(x[1], arr)
+        elseif btype==2
+            I, F = 0.0, 0.0
+            arr1 = [real(dp[i]*dp[i]/p[i]) for i in 1:p_num]  
+            I = trapz(x[1], arr1)
+            arr2 = [real(F_tp[j]*p[j]) for j in 1:p_num]
+            F = trapz(x[1], arr2)
+            I = 1.0/(I+F)
+        end
+        return res
+    else
+        #### multiparameter senario ####
+        xnum = length(x)
+
+        trapzm(x, integrands, slice_dim) =  [trapz(tuple(x...), I) for I in [reshape(hcat(integrands...)[i,:], length.(x)...) for i in 1:slice_dim]] 
+
+        Ip(p,dp) = dp*dp'/p^2
+
+        if btype == 1 
+            integrand(p,dp,rho,drho)=p*pinv(Ip(p,dp)+QFIM(rho,drho,eps))
+            integrands = [integrand(p,dp,rho,drho)|>vec for (p,dp,rho,drho) in zip(p,dp,rho,drho)]
+
+            I = trapzm(x, integrands, xnum^2) |> I->reshape(I,xnum,xnum)
+
+        elseif btype == 2
+            Iprs = [Ip(p,dp)|>vec for (p,dp) in zip(p,dp)]
+            Ipr = trapzm(x, Iprs, xnum^2)|> I->reshape(I,xnum,xnum)
+
+            Fs = [p*QFIM(rho,drho,eps)|>vec for (p,rho,drho) in zip(p,rho,drho)]
+            F = trapzm(x, Fs, xnum^2) |> I->reshape(I,xnum,xnum)
+
+            I = pinv(Ipr+F)
+        end
+        return I
     end
 end
 
 function VTB(x, p, dp, rho, drho; M=[], btype=1, eps=1e-8)
     para_num = length(x)
-    p_num = length(p)
     if para_num == 1
         #### singleparameter senario ####
+        p_num = length(p)
         if M==[]
             M = load_M(size(rho[1])[1])
         end
@@ -130,76 +228,30 @@ function VTB(x, p, dp, rho, drho; M=[], btype=1, eps=1e-8)
         end
         return res
     else
-        #### multiparameter senario ####
-        println("pass")
-    end
-end
+        #### multiparameter senario #### # TODO: load_M for multi-
+        xnum = length(x)
 
-function QVTB(x, p, dp, rho, drho; dtype="SLD", btype=1, eps=1e-8)
-    para_num = length(x)
-    p_num = length(p)
-    if para_num == 1
-        #### singleparameter senario ####
-        if typeof(drho[1]) == Vector{Matrix{ComplexF64}}
-            drho = [drho[i][1] for i in 1:p_num]
-        end
-        if typeof(dp[1]) == Vector{Float64}
-            dp = [dp[i][1] for i in 1:p_num]
-        end
-        F_tp = zeros(p_num)
-        for m in 1:p_num
-            F_tp[m] = QFIM(rho[m], drho[m]; dtype=dtype, eps=eps)
-        end
-        res = 0.0
-        if btype==1
-            I_tp = [real(dp[i]*dp[i]/p[i]^2) for i in 1:p_num]
-            arr = [p[j]/(I_tp[j]+F_tp[j]) for j in 1:p_num]
-            res = trapz(x[1], arr)
-        elseif btype==2
-            I, F = 0.0, 0.0
-            arr1 = [real(dp[i]*dp[i]/p[i]) for i in 1:p_num]  
-            I = trapz(x[1], arr1)
-            arr2 = [real(F_tp[j]*p[j]) for j in 1:p_num]
-            F = trapz(x[1], arr2)
-            res = 1.0/(I+F)
-        end
-        return res
-    else
-        #### multiparameter senario ####
-        println("pass")
-    end
-end
+        trapzm(x, integrands, slice_dim) =  [trapz(tuple(x...), I) for I in [reshape(hcat(integrands...)[i,:], length.(x)...) for i in 1:slice_dim]] 
 
-function TWCB(x, p, dp, rho, drho; eps=1e-8)
-    para_num = length(dp)
-    p_num = length(p)
-    CFIM_res = zeros(para_num,para_num)
-    QFIM_res = zeros(para_num,para_num)
-    
-    deltax = 1.0/p_num
-    LD = [SLD(rho[i], drho[i], eps) for i in 1:p_num]
-    for para_i in 1:para_num
-        for para_j in para_i:para_num
-            arr1 = [real(dp[para_i][i]*dp[para_j][i]/p[i]) for i in 1:p_num]
-            S1 = trapz(x, arr1)
-            CFIM_res[para_i,para_j] = S1
-            CFIM_res[para_j,para_i] = S1
-            
-            F_tp = zeros(p_num)
-            for j in 1:p_num
-                SLD_ac = LD[j][para_i]*LD[j][para_j]+LD[j][para_j]*LD[j][para_i]
-                F_tp[j] = real(0.5*tr(rho[j]*SLD_ac))
-            end
+        Ip(p,dp) = dp*dp'/p^2
 
-            arr2 = [real(F_tp[i]*p[i]) for i in 1:p_num]
-            S2 = trapz(x, arr2)
-            QFIM_res[para_i,para_j] = S2
-            QFIM_res[para_j,para_i] = S2
+        if btype == 1 
+            integrand(p,dp,rho,drho)=p*pinv(Ip(p,dp)+CFIM(rho,drho,M,eps))
+            integrands = [integrand(p,dp,rho,drho)|>vec for (p,dp,rho,drho) in zip(p,dp,rho,drho)]
+
+            I = trapzm(x, integrands, xnum^2) |> I->reshape(I,xnum,xnum)
+
+        elseif btype == 2
+            Iprs = [Ip(p,dp)|>vec for (p,dp) in zip(p,dp)]
+            Ipr = trapzm(x, Iprs, xnum^2)|> I->reshape(I,xnum,xnum)
+
+            Fs = [p*CFIM(rho,drho,M,eps)|>vec for (p,rho,drho) in zip(p,rho,drho)]
+            F = trapzm(x, Fs, xnum^2) |> I->reshape(I,xnum,xnum)
+
+            I = pinv(Ipr+F)
         end
+        return I
     end
-            
-    F_total = CFIM_res + QFIM_res
-    return pinv(F_total)
 end
 
 ########## optimal biased bound ##########
