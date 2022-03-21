c74c28f66800dcd05ddba31e940fa17e15fcedcc
HuaiMing <huaimingyuuu@gmail.com>
2022-03-21 07:35:07 +0000

    revise kraus

--- quanestimation/JuliaSrc/Dynamics/Kraus.jl
+++ quanestimation/JuliaSrc/Dynamics/Kraus.jl
@@ -6,13 +6,13 @@ struct Kraus{R} <: AbstractDynamics
     state_rep::Symbol
 end
 
-struct Kraus_dm <: AbstractDynamicsData
+mutable struct Kraus_dm <: AbstractDynamicsData
     K::AbstractVector
     dK::AbstractVector
     ρ0::AbstractMatrix
 end
 
-struct Kraus_pure <: AbstractDynamicsData
+mutable struct Kraus_pure <: AbstractDynamicsData
     K::AbstractVector
     dK::AbstractVector
     ψ0::AbstractVector
@@ -20,9 +20,13 @@ end
 
 # Constructor for Kraus dynamics
 Kraus(K::AbstractVector, dK::AbstractVector, ρ0::AbstractMatrix) =
-    Kraus{dm}(Kraus_data(K, dK, ρ0), :noiseless, :free, :dm)
+    Kraus{dm}(Kraus_dm(K, dK, ρ0), :noiseless, :free, :dm)
 Kraus(K::AbstractVector, dK::AbstractVector, ψ0::AbstractVector) =
-    Kraus{ket}(Kraus_data(K, dK, ψ0), :noiseless, :free, :ket)
+    Kraus{ket}(Kraus_pure(K, dK, ψ0), :noiseless, :free, :ket)
+Kraus(K::AbstractVector, dK::AbstractMatrix, ρ0::AbstractMatrix) =
+    Kraus{dm}(Kraus_dm(K, [[dK[i,j,:,:][1] for j in 1:size(dK,2)] for i in 1:size(dK,1)], ρ0), :noiseless, :free, :dm)
+Kraus(K::AbstractVector, dK::AbstractMatrix, ψ0::AbstractVector) =
+    Kraus{ket}(Kraus_pure(K,[[dK[i,j,:,:][1] for j in 1:size(dK,2)] for i in 1:size(dK,1)], ψ0), :noiseless, :free, :ket)
 
 function set_state(dynamics::Kraus, state::AbstractVector)
     temp = deepcopy(dynamics)
@@ -38,19 +42,19 @@ end
 
 #### evolution of pure states under time-independent Hamiltonian without noise and controls ####
 function evolve(dynamics::Kraus{ket})
-    (; K, dK, ψ0) = dynamics
+    (; K, dK, ψ0) = dynamics.data
     ρ0 = ψ0 * ψ0'
     ρ = [K * ρ0 * K' for K in K] |> sum
-    dρ = [[dK * ρ0 * K' + K * ρ0 * dK'] |> sum for dK in dK]
+    dρ = [[dK * ρ0 * K' + K * ρ0 * dK' for (K,dK) in zip(K,dK)] |> sum for dK in dK]
 
     ρ, dρ
 end
 
 #### evolution of density matrix under time-independent Hamiltonian without noise and controls ####
 function evolve(dynamics::Kraus{dm})
-    (; K, dK, ρ0) = dynamics
+    (; K, dK, ρ0) = dynamics.data
     ρ = [K * ρ0 * K' for K in K] |> sum
-    dρ = [[dK * ρ0 * K' + K * ρ0 * dK'] |> sum for dK in dK]
+    dρ = [[dK * ρ0 * K' + K * ρ0 * dK' for (K,dK) in zip(K,dK)] |> sum for dK in dK]
 
     ρ, dρ
 end
