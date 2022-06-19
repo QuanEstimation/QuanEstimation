119770f932e468379499db0317ccc7fdfe5c5109
HuaiMing <huaimingyuuu@gmail.com>
2022-06-19 16:07:42 +0800

    update release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup, find_packages
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 with open("HISTORY.rst") as history_file:
@@ -49,6 +49,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
+    long_description_content_type = "text/markdown", 
     include_package_data=True,
     keywords="quanestimation",
     name="quanestimation",
