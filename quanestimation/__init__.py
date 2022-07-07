8583d63bf06f537593362f142d9e9aa9ba29e7ad
HuaiMing <huaimingyuuu@gmail.com>
2022-07-07 13:53:00 +0800

    update release

--- quanestimation/__init__.py
+++ quanestimation/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for quanestimation."""
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 import julia
 import platform
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/QuanEstimation/QuanEstimation",
-    version="0.1.4",
+    version="0.1.5",
     zip_safe=False,
 )
