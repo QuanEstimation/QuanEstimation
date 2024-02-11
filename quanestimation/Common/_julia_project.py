deb1963cc5d42fb74af2ba3d4a708fa2baef2004
hmyuuu <HuaimingYuuu@gmail.com>
2024-02-11 15:31:56 +0800

    fix patching

--- quanestimation/Common/_julia_project.py
+++ quanestimation/Common/_julia_project.py
@@ -1,10 +1,29 @@
 
-import logging
-import julia_project, julia_project_basic
+import os, logging, platform
+# import julia_project, julia_project_basic
 from julia_project import JuliaProject
+from julia_project_basic.basic import run_pkg_commands, run_julia
 from pathlib import Path
 
 pkg_path = Path(__file__).parent.parent
+
+def run_pkg_commands_monkey_patch(project_path, commands, julia_exe=None, depot_path=None, clog=False, no_stderr=False):
+    if not os.path.isdir(project_path) and not os.path.isfile(project_path):
+        raise FileNotFoundError(f"{project_path} does not exist")
+    if os.path.isfile(project_path):
+        project_path = os.path.dirname(project_path) # julia commands run 250ms faster if we do this. why?
+    com = f'import Pkg; Pkg.activate(raw"{project_path}"); ' + commands
+    return run_julia(commands=com, julia_exe=julia_exe, depot_path=depot_path, clog=clog, no_stderr=no_stderr)
+
+def _load_julia_utils_monkey_patch(self):
+        srcdir = os.path.dirname(os.path.realpath(__file__))
+        utilf = os.path.join(srcdir, "utils.jl")
+        self.calljulia.seval(f'include(raw"{utilf}")')
+
+## Remove munual monkey patch when fixed
+
+run_pkg_commands = run_pkg_commands_monkey_patch
+JuliaProject._load_julia_utils = _load_julia_utils_monkey_patch
 
 project = JuliaProject(
     name="quanestimation",
@@ -18,19 +37,3 @@ project = JuliaProject(
     # post_init_hook=_post_init_hook, # Run this after ensure_init
     calljulia = "juliacall"
 )
-
-def run_pkg_commands_monkey_patch(project_path, commands, julia_exe=None, depot_path=None, clog=False, no_stderr=False):
-    if not os.path.isdir(project_path) and not os.path.isfile(project_path):
-        raise FileNotFoundError(f"{project_path} does not exist")
-    if os.path.isfile(project_path):
-        project_path = os.path.dirname(project_path) # julia commands run 250ms faster if we do this. why?
-    com = f'import Pkg; Pkg.activate(raw"{project_path}"); ' + commands
-    return run_julia(commands=com, julia_exe=julia_exe, depot_path=depot_path, clog=clog, no_stderr=no_stderr)
-
-def _load_julia_utils_monkey_patch(self):
-        srcdir = os.path.dirname(os.path.realpath(__file__))
-        utilf = os.path.join(srcdir, "utils.jl")
-        self.calljulia.seval(f'include("{utilf}")')
-      
-julia_project_basic.run_pkg_commands = run_pkg_commands_monkey_patch
-julia_project._load_julia_utils = _load_julia_utils_monkey_patch
