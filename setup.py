9cda5d7f416b9744238f12731e653207826f3a38
Jing Liu <liujing@hainanu.edu.cn>
2025-06-13 19:07:24 +0800

    release v0.2.6

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/QuanEstimation/QuanEstimation",
-    version="0.2.5",
+    version="0.2.6",
     zip_safe=False,
 )
