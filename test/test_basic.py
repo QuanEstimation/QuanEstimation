73fc148085530ba80ae65df04cd6dce8bb88b3af
Jing Liu <liujing@hainanu.edu.cn>
2025-06-17 17:06:04 +0800

    modify test_basic

--- test/test_basic.py
+++ test/test_basic.py
@@ -4,20 +4,18 @@ from quanestimation.Common.Common import basis, gramschmidt, suN_generator
 
 def test_basis():
     result = basis(2, 0)
-    assert np.array_equal(result, np.array([[1], [0]])) == 1
+    assert np.allclose(result, np.array([[1], [0]])) == 1
 
 def test_gramschmidt():
-    A = np.array([[1, 0], 
-                  [1, 1]], dtype=np.complex128)
+    A = np.array([[1., 0.], [1., 1.]], dtype=np.complex128)
     result = gramschmidt(A)
-    assert np.array_equal(result, np.array([[1.+0.j, 0.+0.j], [0.+0.j, 1.+0.j]])) == 1
+    assert np.allclose(result, np.array([[1.+0.j, 0.+0.j],[0.+0.j, 1.+0.j]])) == 1
 
 def test_suN_generator():
     n = 2
     result = suN_generator(n)
-    expected = (np.array([[1, 0], [0, -1]]), 
-                np.array([[0, 1], [1, 0]]), 
-                np.array([[0, 0], [0, 1]]))
-    assert (np.array_equal(result[0], expected[0]) and 
-            np.array_equal(result[1], expected[1]) and 
-            np.array_equal(result[2], expected[2])) == 1    
+    sx = np.array([[0., 1.], [1., 0.]], dtype=np.complex128) 
+    sy = np.array([[0., -1j], [1j, 0.]], dtype=np.complex128)
+    sz = np.array([[1., 0.j], [0.j, -1.]], dtype=np.complex128)
+    assert (np.allclose(result[0], sx) and np.allclose(result[1], sy) and \
+            np.allclose(result[2], sz)) == 1    
