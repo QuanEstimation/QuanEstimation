d6e7428e19dabb8621ec3843e925a76d9999ae4f
Jing Liu <liujing@hainanu.edu.cn>
2025-06-17 19:05:24 +0800

    test julia_project

--- quanestimation/Common/_julia_project.py
+++ quanestimation/Common/_julia_project.py
@@ -5,7 +5,7 @@ from julia_project import JuliaProject
 from julia_project_basic.basic import run_julia
 from pathlib import Path
 
-os.environ["JULIA_PROJECT_COMPILE"] = "yes" # Force Julia to compile the project when it is first loaded.
+os.environ["JULIA_PROJECT_COMPILE"] = "no" 
 
 pkg_path = Path(__file__).parent.parent
 
