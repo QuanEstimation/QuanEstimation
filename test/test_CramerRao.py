092aae38efa98915f9ce9fa3892adf04a110d189
Jing Liu <liujing@hainanu.edu.cn>
2025-06-26 19:54:11 +0800

    add function test_QFIM_Bloch

--- test/test_CramerRao.py
+++ test/test_CramerRao.py
@@ -1,5 +1,5 @@
 import pytest
-from quanestimation.AsymptoticBound.CramerRao import QFIM, CFIM, QFIM_Kraus
+from quanestimation.AsymptoticBound.CramerRao import QFIM, CFIM, QFIM_Kraus, QFIM_Bloch
 import numpy as np
 
 def test_CramerRao():
@@ -34,8 +34,9 @@ def test_QFIM_Kraus():
     # Kraus operator
     K0 = np.array([[1, 0], [0, np.sqrt(0.5)]])
     K1 = np.array([[np.sqrt(0.5), 0], [0, 0]])
-    K = [K0, K1]  # list of Kraus operators
-    # derivative of the Kraus operator w.r.t. a parameter
+    # list of Kraus operators
+    K = [K0, K1]  
+    # derivatives of the Kraus operator w.r.t. the parameter
     dK = [[np.array([[0, 0], [0, -0.5/np.sqrt(0.5)]])], [np.array([[0, 0.5/np.sqrt(0.5)], [0, 0]])]]
     # probe state
     rho0 = 0.5*np.array([[1., 1.], [1., 1.]]) 
@@ -43,3 +44,23 @@ def test_QFIM_Kraus():
     result = QFIM_Kraus(rho0, K, dK)
     # check the result
     assert np.allclose(result, 1.5) == 1    
+
+def test_QFIM_Bloch():
+    """
+    Test the Quantum Fisher Information Matrix (QFIM) for a Bloch vector representation.
+    This test checks the calculation of the QFIM for a specific Bloch vector and its derivatives.
+    """
+    # Bloch vector
+    theta = np.pi/4
+    phi = np.pi/2
+    eta = 0.8
+    b = eta*np.array([np.sin(2*theta)*np.cos(phi), np.sin(2*theta)*np.sin(phi), np.cos(2*theta)])
+    # derivatives of the Bloch vector w.r.t. theta, phi
+    db_theta = eta*np.array([2*np.cos(2*theta)*np.cos(phi), 2*np.cos(2*theta)*np.sin(phi), -2*np.sin(2*theta)])
+    db_phi = eta*np.array([-np.sin(2*theta)*np.sin(phi), np.sin(2*theta)*np.cos(phi), 0])
+    # list of derivatives
+    db = [db_theta, db_phi] 
+    # calculate the QFIM
+    result = QFIM_Bloch(b, db)
+    # check the result
+    assert np.allclose(result, np.array([[4.*eta**2, 0.], [0., eta**2*np.sin(2*theta)**2]])) == 1
