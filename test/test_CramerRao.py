1bebc798759050c8080b5a9e76957703e14cccb4
Jing Liu <liujing@hainanu.edu.cn>
2025-06-27 20:59:29 +0800

    add test_QFIM_Gauss, julia deps bug

--- test/test_CramerRao.py
+++ test/test_CramerRao.py
@@ -1,5 +1,5 @@
 import pytest
-from quanestimation.AsymptoticBound.CramerRao import QFIM, CFIM, QFIM_Kraus, QFIM_Bloch
+from quanestimation.AsymptoticBound.CramerRao import QFIM, CFIM, QFIM_Kraus, QFIM_Bloch, QFIM_Gauss, LLD, RLD
 import numpy as np
 
 def test_CramerRao():
@@ -64,3 +64,71 @@ def test_QFIM_Bloch():
     result = QFIM_Bloch(b, db)
     # check the result
     assert np.allclose(result, np.array([[4.*eta**2, 0.], [0., eta**2*np.sin(2*theta)**2]])) == 1
+
+def test_QFIM_Gauss():
+    """
+    Test the Quantum Fisher Information Matrix (QFIM) for the Gaussian state representation.
+    This test checks the calculation of the QFIM for a specific Gaussian state and its derivatives.
+    The example is in J. Phys. A: Math. Theor. 52, 035304 (2019).
+    """
+    # Gaussian state parameters
+    r = 1.  # squeezing parameter
+    beta = 0.5
+    lamb = np.cosh(0.5*beta)/np.sinh(0.5*beta)  # lambda parameter
+    mu = np.array([0., 0.])  # mean vector
+    sigma = lamb*np.array([[np.cosh(2*r), -np.sinh(2*r)], [-np.sinh(2*r), np.cosh(2*r)]])  # covariance matrix
+    # derivatives of the Gaussian state w.r.t. the parameters
+    dmu = [np.array([0., 0.])]  # derivatives of mean vector
+    dsigma = [np.array([[np.sinh(2*r), -np.cosh(2*r)], [-np.cosh(2*r), np.sinh(2*r)]])]  # derivatives of covariance matrix
+    # calculate the QFIM
+    result = QFIM_Gauss(mu, sigma, dmu, dsigma)
+    # check the result
+    assert np.allclose(result, np.array([[(lamb**2-1)/4., 0.], [0., 4*lamb**2/(lamb**2+1)]])) == 1   
+
+# def test_LLD():
+#     """
+#     Test the left logarithmic derivative (LLD) for a specific parameterized quantum state.
+#     This test checks the calculation of the LLD for a specific state and its derivatives.
+#     """
+#     # parameterized state
+#     theta = np.pi/4
+#     phi = np.pi/4
+#     rho = np.array([[np.cos(theta)**2, np.cos(theta)*np.sin(theta)*np.exp(-1j*phi)], 
+#                      [np.cos(theta)*np.sin(theta)*np.exp(1j*phi), np.sin(theta)**2]])
+#     # derivative of the state w.r.t. theta, phi
+#     drho = [np.array([[-np.sin(2*theta), 2*np.cos(2*theta)*np.exp(-1j*phi)], 
+#                       [2*np.cos(2*theta)*np.exp(1j*phi), np.sin(2*theta)]]),
+#             np.array([[0, -1j*np.cos(theta)*np.sin(theta)*np.exp(-1j*phi)], 
+#                       [1j*np.cos(theta)*np.sin(theta)*np.exp(1j*phi), 0]])] 
+#     # calculate the LLD
+#     result_o = LLD(rho, drho, rep="original")
+#     result_e = LLD(rho, drho, rep="eigen")
+
+#     # check the result
+#     assert np.allclose(result_o, np.array([[4., 0.], [0., 0.]])) == 1
+#     assert np.allclose(result_e, np.array([[4., 0.], [0., 0.]])) == 1
+
+# def test_RLD():
+#     """
+#     Test the right logarithmic derivative (RLD) for a specific parameterized quantum state.
+#     This test checks the calculation of the RLD for a specific state and its derivatives.
+#     """
+#     # parameterized state
+#     theta = np.pi/4
+#     phi = np.pi/4
+#     rho = np.array([[np.cos(theta)**2, np.cos(theta)*np.sin(theta)*np.exp(-1j*phi)], 
+#                      [np.cos(theta)*np.sin(theta)*np.exp(1j*phi), np.sin(theta)**2]])
+#     # derivative of the state w.r.t. theta, phi
+#     drho = [np.array([[-np.sin(2*theta), 2*np.cos(2*theta)*np.exp(-1j*phi)], 
+#                       [2*np.cos(2*theta)*np.exp(1j*phi), np.sin(2*theta)]]),
+#             np.array([[0, -1j*np.cos(theta)*np.sin(theta)*np.exp(-1j*phi)], 
+#                       [1j*np.cos(theta)*np.sin(theta)*np.exp(1j*phi), 0]])] 
+#     # calculate the RLD
+#     result_o = RLD(rho, drho, rep="original")
+#     result_e = RLD(rho, drho, rep="eigen")
+
+#     # check the result
+#     assert np.allclose(result_o, np.array([[4., 0.], [0., 0.]])) == 1
+#     assert np.allclose(result_e, np.array([[4., 0.], [0., 0.]])) == 1    
+
+
